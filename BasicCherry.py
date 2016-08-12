d6aab179fb21ce2d503ad1756245bac8b2180add
Brig Young <Sonophoto@users.noreply.github.com>
2016-08-12 14:20:48 -0700

    Update BasicCherry.py

--- BasicCherry.py
+++ BasicCherry.py
@@ -3,7 +3,7 @@ import cherrypy
 class Root(object):
     @cherrypy.expose
     def index(self):
-        return "Hello World"
+        return "Greetings from the Cherrypy application"
 
 if __name__ == "__main__":
     cherrypy.quickstart(Root(), '/')
